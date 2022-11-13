86f6091cca0d8e4d58a5f84ab70288a5f2f3dffa
Jtility <leejewon97@gmail.com>
2022-11-14 06:22:13 +0900

    refactor(Application.kt): delete class PrintAndInput and make fun printAndInput
    delete class PrintAndInput and make fun printAndInput

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     TODO("프로그램 구현")
+}
+
+fun printAndInput(what: String): String {
+	val postposition = when (what) {
+		"구입금액" -> "을"
+		"당첨 번호", "보너스 번호" -> "를"
+		else -> throw IllegalArgumentException("[ERROR] inappropriate order")
+	}
+	println("$what$postposition 입력해 주세요.")
+	return Console.readLine()
 }
